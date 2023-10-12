893d99bf2545081dd7b38701f2120d4f76114c1c
Rodrigo Valadao <rodrigomarciosv@gmail.com>
2023-10-12 16:11:01 +1000

    Initital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState, useEffect } from "react";
+import "./App.css";
+import { HomeScreen, ListScreen } from "./screens";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import { ListCard } from "./components/ListCard";
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="/list" element={<ListScreen />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+/* 
+
+const fakeList = [
+  {
+    id: 0,
+    name: "Arroz",
+    quantity: 2,
+    checked: false,
+  },
+  {
+    id: 1,
+    name: "Feijão",
+    quantity: 1,
+    checked: false,
+  },
+  {
+    id: 2,
+    name: "Macarrão",
+    quantity: 4,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Tomate",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [marketList, setMarketList] = useState([]);
+
+  const loadList = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setMarketList(fakeList);
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    loadList();
+  }, []);
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        marketList.map((item) => (
+          <ListCard key={`item_${item.id}`} item={item} />
+        ))
+      )}
     </div>
   );
 }
 
 export default App;
+
+**/
---
+++ src/screens/Home/index.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+
+import { Button, Input, ListCard } from "../../components";
+
+export const HomeScreen = () => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+
+  const onClickContinue = () => {
+    // alert("Clicked");
+    if (userName.length < 3) {
+      alert("User name should have at least 3 characters");
+      return;
+    }
+    localStorage.setItem("@supermarket:username", userName);
+    navigate("/list");
+  };
+
+  return (
+    <div className="home-screen-container">
+      <div className="home-screen-content-container">
+        <img
+          className="shopping-bag-image"
+          src="/images/shopping-bag.svg"
+          alt="Shopping bag with vegetables and fruits"
+        ></img>
+        <h1 className="home-screen-title">
+          Your grocery list easier than ever
+        </h1>
+        <h3 className="home-screen-subtitle">
+          We help you organize your shopping list in the best way
+        </h3>
+        <h3 className="home-screen-subtitle-description">
+          Please type your user name below to have access to your shopping list
+        </h3>
+        <Input
+          onChange={(text) => setUserName(text)}
+          value={userName}
+          label="UserName"
+          placeholder="Type your user name"
+        />
+        <div className="home-screen-button-container">
+          <Button onClick={onClickContinue}>Continue</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
